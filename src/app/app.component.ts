6b4f6e2176d1c42f9dfccec2c919e82f20ddc860
naiksunit2011 <naiksunit2011@gmail.com>
2019-02-27 23:58:22 +0530

    flags added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,42 @@ export class AppComponent {
   @ViewChild('sidenav') sidenav: MatSidenav;
   countryArr = [];
   currentCountryCode = "INR";
-  countryJson = { "RUB":{"currency":"Russian ruble"},"RON":{"currency":"Romanian leu"},"BGN":{"currency":"Bulgarian lev"},"EUR":{"currency":"Euro"},"TRY":{"currency":"Turkish lira"},"AUD":{"country":"","currency":"Australian dollar"},"MXN":{"country":"","currency":"Mexican peso"},"USD": { "country": "Virgin Islands (US)", "currency": "US Dollar" }, "NOK": { "country": "Svalbard and Jan Mayen", "currency": "Norwegian Krone" }, "BRL": { "country": "Brazil", "currency": "Brazil Real" }, "CAD": { "country": "Canada", "currency": "Canadian Dollar" }, "CNY": { "country": "China", "currency": "Yuan Renminbi" }, "HKD": { "country": "China (Hong Kong S.A.R.)", "currency": "Hong Kong Dollar" }, "NZD": { "country": "Tokelau", "currency": "New Zealand Dollar" }, "HRK": { "country": "Croatia", "currency": "Croatian Kuna" }, "CZK": { "country": "Czech Republic", "currency": "Czech Koruna" }, "DKK": { "country": "Greenland", "currency": "Danish Krone" }, "HUF": { "country": "Hungary", "currency": "Forint" }, "ISK": { "country": "Iceland", "currency": "Iceland Krona" }, "INR": { "country": "India", "currency": "Indian Rupee" }, "IDR": { "country": "Indonesia", "currency": "Indonesian Rupiah" }, "ILS": { "country": "Israel", "currency": "New Israeli Sheqel" }, "JPY": { "country": "Japan", "currency": "Yen" }, "KRW": { "country": "Korea, Republic of", "currency": "South Korean Won" }, "CHF": { "country": "Switzerland", "currency": "Swiss Franc" }, "MYR": { "country": "Malaysia", "currency": "Malaysian Ringgit" }, "PHP": { "country": "Philippines", "currency": "Philippine Peso" }, "PLN": { "country": "Poland", "currency": "Poland Zloty" }, "SGD": { "country": "Singapore", "currency": "Singapore Dollar" }, "ZAR": { "country": "South Africa", "currency": "South African Rand" }, "SEK": { "country": "Sweden", "currency": "Swedish Krona" }, "THB": { "country": "Thailand", "currency": "Thai Baht" }, "GBP": { "country": "United Kingdom", "currency": "Pound Sterling" } };
-  displayedColumns = ["code","name","rate"];
+  countryJson = { 
+                  "RUB": { "country": "Russia","currency":"Russian ruble"},
+                  "RON": { "country": "Romania","currency":"Romanian leu"},
+                  "BGN": { "country": "Bulgaria","currency":"Bulgarian lev"},
+                  "EUR": { "country": "Germany","currency":"Euro"},
+                  "TRY": { "country": "Turkey","currency":"Turkish lira"},
+                  "AUD": { "country": "Australia","currency":"Australian dollar"},
+                  "MXN": { "country": "Mexico","currency":"Mexican peso"},
+                  "USD": { "country": "United States of America", "currency": "US Dollar" }, 
+                  "NOK": { "country": "Norway", "currency": "Norwegian Krone" }, 
+                  "BRL": { "country": "Brazil", "currency": "Brazil Real" }, 
+                  "CAD": { "country": "Canada", "currency": "Canadian Dollar" }, 
+                  "CNY": { "country": "China", "currency": "Yuan Renminbi" }, 
+                  "HKD": { "country": "Hong Kong", "currency": "Hong Kong Dollar" }, 
+                  "NZD": { "country": "New Zealand", "currency": "New Zealand Dollar" }, 
+                  "HRK": { "country": "Croatia", "currency": "Croatian Kuna" }, 
+                  "CZK": { "country": "Czech Republic", "currency": "Czech Koruna" }, 
+                  "DKK": { "country": "Greenland", "currency": "Danish Krone" }, 
+                  "HUF": { "country": "Hungary", "currency": "Forint" }, 
+                  "ISK": { "country": "Iceland", "currency": "Iceland Krona" }, 
+                  "INR": { "country": "India", "currency": "Indian Rupee" }, 
+                  "IDR": { "country": "Indonesia", "currency": "Indonesian Rupiah" }, 
+                  "ILS": { "country": "Israel", "currency": "New Israeli Sheqel" }, 
+                  "JPY": { "country": "Japan", "currency": "Yen" }, 
+                  "KRW": { "country": "South Korea", "currency": "South Korean Won" }, 
+                  "CHF": { "country": "Switzerland", "currency": "Swiss Franc" }, 
+                  "MYR": { "country": "Malaysia", "currency": "Malaysian Ringgit" }, 
+                  "PHP": { "country": "Philippines", "currency": "Philippine Peso" }, 
+                  "PLN": { "country": "Poland", "currency": "Poland Zloty" }, 
+                  "SGD": { "country": "Singapore", "currency": "Singapore Dollar" }, 
+                  "ZAR": { "country": "South Africa", "currency": "South African Rand" }, 
+                  "SEK": { "country": "Sweden", "currency": "Swedish Krona" }, 
+                  "THB": { "country": "Thailand", "currency": "Thai Baht" }, 
+                  "GBP": { "country": "United Kingdom", "currency": "Pound Sterling" } 
+                };
+  displayedColumns = ["flag","code","name","rate"];
   
   constructor(private appService: AppService,private util : UtilService) {
     this.countryArr = Object.keys(this.countryJson);
@@ -32,7 +66,15 @@ export class AppComponent {
     var latest = this.appService.loadLatest(this.currentCountryCode).subscribe(
       (data: { status: string, response: any }) => {
         if (data) {
-          this.latestRate = this.util.objToArray(data["rates"],"code","rate");
+          let rates = data["rates"];
+          let newRate = {};
+          newRate[this.currentCountryCode] = rates[this.currentCountryCode];
+          delete rates[this.currentCountryCode];
+          let keys = Object.keys(rates);
+          for(let k of keys){
+            newRate[k] = rates[k];
+          }
+          this.latestRate = this.util.objToArray(newRate,"code","rate");
         }
       },
       (error) => {
@@ -46,6 +88,10 @@ export class AppComponent {
     if(this.countryJson[code])
       currency = this.countryJson[code].currency;
     return currency;
+  }
+
+  getFlagPath(name){
+    return "../assets/flags/"+name+".png";
   }
 
 }
